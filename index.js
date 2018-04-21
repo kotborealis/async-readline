f11d301821e5dfd9da2fa8e5b62678ddc6d987e9
kotborealis <kotborealis@gmail.com>
2018-04-22 00:08:48 +0300

    added missing methods

--- index.js
+++ index.js
@@ -10,4 +10,11 @@ module.exports = function(options = DEFAULT_RD_OPTIONS){
     const r = readline.createInterface(options);
     this.line = () => new Promise(resolve => r.once('line', resolve));
     this.question = q => new Promise(resolve => r.question(q, resolve));
+
+    this.close = () => r.close();
+    this.pause = () => r.pause();
+    this.resume = () => r.resume();
+    this.prompt = (...args) => r.prompt(...args);
+    this.setPrompt = (...args) => r.setPrompt(...args);
+    this.write = (...args) => r.write(...args);
 };
